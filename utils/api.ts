c30c9f276769c227b7ac2935232e2fb13a33fbc7
JustFossa <panko.marek@icloud.com>
2024-11-22 16:32:53 +0100

    fixed api utils

--- utils/api.ts
+++ utils/api.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	headers: {
-		"Content-Type": "application/json",
-	},
-});
-
 export const createNote = async (title: string, content: string) => {
-	const { data } = await api.post("/note", { title, content });
+	const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/note`, {
+		title,
+		content,
+	});
 	return data;
 };
 
 export const getNote = async (id: string) => {
-	const { data } = await api.get(`/note/${id}`);
+	const { data } = await axios.get(
+		`${process.env.NEXT_PUBLIC_API_URL}/note/${id}`
+	);
 	return data;
 };
