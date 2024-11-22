c76ed39dcde864f4089dbf52a2ce2189f04b8d78
JustFossa <panko.marek@icloud.com>
2024-11-22 19:13:21 +0100

    added note deleted page

--- app/note/[id]/page.tsx
+++ app/note/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import NoteBlock from "@/components/Note";
 import { getNote } from "@/utils/api";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
 
@@ -14,6 +15,7 @@ type Note = {
 export default function Note({ params }: { params: Promise<{ id: string }> }) {
 	const router = useRouter();
 	const unwrapped = use(params);
+	const [deleted, setDeleted] = useState<boolean>(false);
 	const [note, setNote] = useState<Note | null>(null);
 
 	useEffect(() => {
@@ -22,13 +24,28 @@ export default function Note({ params }: { params: Promise<{ id: string }> }) {
 				setNote(note.data);
 			})
 			.catch((err) => {
-				router.push("/");
+				if (err.status === 403) {
+					setDeleted(true);
+				} else {
+					router.push("/");
+				}
 			});
 	}, [unwrapped.id]);
 
 	return (
 		<div className="items-center self-center w-[80%] md:w-[60%] sm:w-[70%] lg:w-[60%] xl:w-[50%] h-full flex flex-col">
 			{note && <NoteBlock title={note.title} content={note.content} />}
+			{deleted && (
+				<div className="bg-secondary p-5 rounded-xl flex flex-col w-full sm:w-[90%] md:w-[75%] lg:w-[60%] gap-y-2 mt-[10%] shadow-lg">
+					<span className="text-medium bg-background rounded-md p-2">
+						The note you are looking for has already been viewed and is gone for
+						good.
+					</span>
+					<Link className="hover:underline self-center" href="/">
+						Click here to create a new note
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 }
